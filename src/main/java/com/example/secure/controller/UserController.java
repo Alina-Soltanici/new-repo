c994b06aaa59dc0cf84d578c072439b3b8c815b2
alinalinux <soltanicialina777@gmail.com>
2024-10-13 12:13:47 +0300

    added lasrt

--- src/main/java/com/example/secure/controller/UserController.java
+++ src/main/java/com/example/secure/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//lasrt
 @RestController
 @RequestMapping("/user")
 @CrossOrigin("*")
